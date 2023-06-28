07d4b68d602a4a95a4dd573f5b272163cad05852
jarcopolo <jarrito1337@gmail.com>
2023-06-28 21:20:12 +0200

    UI update

--- src/main/java/jarcopolo/invitations/App.java
+++ src/main/java/jarcopolo/invitations/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("ui/style.fxml"));
-        stage.setScene(new Scene(root, 800, 600));
+        stage.setScene(new Scene(root, 1200, 700));
         stage.show();
 
     }
